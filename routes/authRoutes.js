8fb29466abf3a2156d79278997a458138613c3e6
himanshush200599 <himanshush214@gmail.com>
2018-11-05 09:04:57 -0500

    done with all routes of  authentication and added mongoDB

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,10 @@ module.exports = app => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get("/auth/api/logout", (req, res) => {
+    req.logout();
+  });
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,17 @@
 const passport = require("passport");
 const keys = require("../config/keys");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 passport.use(
   new GoogleStrategy(
     {
@@ -10,9 +20,18 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", accessToken);
-      console.log("Profile:", profile);
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //means that we already have the uuser in our  database
+          done(null, existingUser);
+        } else {
+          new User({
+            googleID: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
