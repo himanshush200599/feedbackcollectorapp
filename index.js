dee932b9fdc0e9e5bf35b305e1c0dc40372a4f29
himanshush200599 <himanshush214@gmail.com>
2018-11-05 01:34:29 -0500

    added google authentication and break down index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send({
-    hi: "hello himanshu sharma"
-  });
-});
+require("./services/passport");
+require("./routes/authRoutes")(app);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
