62c78b9355fe5e5c62e6d3e7357fd735000eb554
himanshush200599 <himanshush214@gmail.com>
2018-11-12 01:16:05 -0500

    done with header and authentication

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 class Landing extends Component {
   render() {
-    return <div>Landing page</div>;
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h3>FEEDBACK COLLECTOR APP</h3>
+        <p>Collect feedback from your users</p>
+      </div>
+    );
   }
 }
 export default Landing;
