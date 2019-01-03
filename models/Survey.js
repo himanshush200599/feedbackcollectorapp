b5758044db68362d45efb9533d72b166acf84873
himanshush200599 <himanshush214@gmail.com>
2019-01-03 13:41:07 -0500

    fixes all bugs and running correctly till  this stage

--- models/Survey.js
+++ models/Survey.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const receiptSchema = require("./Receipt");
+const RecipientSchema = require("./Receipt");
+
 const surveySchema = new Schema({
   title: String,
-  subject: String,
   body: String,
-  recipients: [receiptSchema],
-  yes: { type: Boolean, default: 0 },
-  no: { type: Boolean, default: 0 },
+  subject: String,
+  recipients: [RecipientSchema],
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
   _user: { type: Schema.Types.ObjectId, ref: "User" },
   dateSent: Date,
   lastResponded: Date
 });
-mongoose.model("surveys", "surveySchema");
+
+mongoose.model("surveys", surveySchema);
