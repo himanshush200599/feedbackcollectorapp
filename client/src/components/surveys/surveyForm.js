f26eb0d8bb98f460cf11ae6cc5fa7f9a4c499862
himanshush200599 <himanshush214@gmail.com>
2019-01-04 14:08:30 -0500

    make reusable field component

--- client/src/components/surveys/surveyForm.js
+++ client/src/components/surveys/surveyForm.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import SurveyField from "./surveyField";
 class surveyForm extends Component {
+  renderFields() {
+    return (
+      <div>
+        <Field
+          type="text"
+          label="Survey Title"
+          name="title"
+          component={SurveyField}
+        />
+        <Field
+          type="text"
+          label="Subject Line"
+          name="subject"
+          component={SurveyField}
+        />
+        <Field
+          type="text"
+          label="Email body"
+          name="body"
+          component={SurveyField}
+        />
+        <Field
+          type="text"
+          label="Recipient List"
+          name="emails"
+          component={SurveyField}
+        />
+      </div>
+    );
+  }
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text" name="surveyTitle" component="input" />
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>
