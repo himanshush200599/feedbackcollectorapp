e80762746ca4acf26a24cb132146d231882cf836
himanshush200599 <himanshush214@gmail.com>
2019-01-09 12:30:43 -0500

    validate title field

--- client/src/components/surveys/surveyField.js
+++ client/src/components/surveys/surveyField.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default ({ input, label }) => {
+export default ({ input, label, meta: { touched, error } }) => {
   return (
     <div>
       <label>{label} </label>
       <input {...input} />
+      {touched && error}
     </div>
   );
 };
